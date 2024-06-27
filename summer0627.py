eec3cd5c3062ed4455d929ffe0385d371506a485
adorahelen <adorahelenmin@gmail.com>
2024-06-27 10:04:39 +0900

    can be?

---
+++ summer0627.py
@@ -0,0 +1,49 @@
+
+# a = int(input(""))
+# b = int(input(""))
+# c = int(input(""))
+
+# print(f"{a} + {b} + {c} = {a+b+c}")
+# print(f"{a} - {b} - {c} = {a-b-c}")
+# print(f"{a} * {b} * {c} = {a*b*c}")
+# print(f"{a} + {b} + {c} = {a+b+c}")
+
+# print("%d + %d + %d = %d" %(a, b, c, a+b+c))
+# print("%d - %d - %d = %d" %(a, b, c, a-b-c))
+# print("%d * %d * %d = %d" %(a, b, c, a*b*c))
+# print("%s / %s = %s" %(a, b, a/b))
+# print("%d // %d = %d" %(a, b, a//b))
+# print("%d %% %d = %d"%(a, b, a%b))
+# print("%d ^ %d = %d"%(a, b, a**b))
+# print("%s" %(min(a,b,c)))
+# print("%s" %(max(a,b,c)))
+
+# print("%d"%(a) ,end="") 줄바꿈 안하도록
+
+# mon = int(input())
+
+# mon_10000 = mon // 10000
+# mon_1000 = (mon % 10000) // 1000
+# mon_100 = (mon % 1000) // 100
+# mon_10 = (mon % 100) // 10
+
+# print(f"만원짜리 : {mon_10000} 개")
+# print(f"천원짜리 : {mon_1000} 개")
+# print(f"백원짜리 : {mon_100} 개")
+# print(f"십원짜리 : {mon_10} 개")
+
+#원기둥의 부피를 계산하는 프로그램 
+# V = 3.141592
+# Radius = float(input("원의 반지름을 입력하시오 : "))
+# High = float(input("원의 높이를 입력하시오 : "))
+# result = V * (Radius**2) * High
+
+# print("%.2f"%(result))
+# print(f"{result:.2f}")
+# print("{:.2f}".format(result))
+# print("어려워"+str(round(result, 2)))
+# print("아", result)
+# print("아아", round(result, 2))
+
+print(1+2j-3j)
+
