be8a4b5f1d414656077a50dfd56998043cc6c17a
adorahelen <adorahelenmin@gmail.com>
2024-07-03 09:35:25 +0900

    see book is better than typing

---
+++ summer0703midterm.py
@@ -0,0 +1,23 @@
+# for i in range (5):
+   
+#     print("aaaaa", end=" ")
+#     #range (start, stop, step)
+
+# for i in range (1, 6, 1):
+#     print(i, end=" ")
+
+# sum = 0
+# for i in range (1, 101, 1):
+#     sum += i
+#     print(sum)
+
+# fac = 1
+# n = int(input(" : "))
+# for a in range(1, n+1):
+#     fac = fac * a
+# print(" 입력값의 팩토리얼은 ", fac)
+
+response = True
+while response != "그래":
+    response = input(" : ")
+print("탈출") 
