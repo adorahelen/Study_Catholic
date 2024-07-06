ca32e69e6f2144266a8c31105bf537cbf6ebc48a
adorahelen <adorahelenmin@gmail.com>
2024-07-06 16:47:01 +0900

    sadsad

---
+++ summer0706.py
@@ -0,0 +1,13 @@
+for i in range (10):
+    lista = []
+    lista.append("s")
+    lista.append("K")
+print(lista)
+b= "".join(lista)
+print(b)
+
+slist = ["duddj" , "asdad", "asdasd", "sadasd"]
+print(slist)
+
+a = "".join(slist)
+print(a)
