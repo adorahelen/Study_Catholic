f09d9417f02bb33d4883e0b6172442e2a4f8d221
adorahelen <adorahelenmin@gmail.com>
2024-07-08 10:35:08 +0900

    hunggry still always

---
+++ summer0708.py
@@ -0,0 +1,107 @@
+import turtle, random
+
+
+# t = turtle.Turtle()
+
+# dice1 = 'img/dice1.gif'
+# dice2 ='img/dice2.gif'
+# dice3 ='img/dice3.gif'
+# dice4 = 'img/dice4.gif'
+# dice5 ='img/dice5.gif'
+# dice6 ='img/dice6.gif'
+# scr = turtle.Screen()
+# scr2 = turtle.Screen()
+# scr.addshape(dice1)
+# scr.addshape(dice2)
+# scr.addshape(dice3)
+# scr.addshape(dice4)
+# scr.addshape(dice5)
+# scr.addshape(dice6)
+# lista = [dice1, dice2, dice3, dice4, dice5, dice6]
+
+# while True:
+#     a= random.choice(lista)
+#     b= random.choice(lista)
+#     if a == b :break
+#     else : continue 
+
+# window = turtle.Screen()
+# window.exitonclick()
+
+# import random, turtle
+# scr = turtle.Screen()
+# t = turtle.Turtle()
+# dice = []
+# for i in range(6):
+#     dice.append(f"img/dice[i+1].gif")
+#     scr.addshape(dice[i])
+# cnt = 1
+
+# while True:
+#     f_dice = random.randint(1, 6)
+#     s_dice = random.randint(1, 6)
+#     t.up()
+#     t.goto(-100, 0)
+#     t.shape(dice[f_dice-1])
+#     t.stamp()
+#     t.goto(100, 0)
+#     t.shape(dice[f_dice-1])
+#     t.stamp()
+#     if :
+#     else:
+
+# lista = [0] * 7
+# lista2 = [0] * 7
+# #리스트 선언시 곱하기 추가해주기
+
+# i = 0
+# for i in range(i, 7):
+
+#     lista[i] = random.randint(50, 100)
+#     lista2[i] = random.randint(50, 100)
+
+# for i in range (7):
+#     if lista[i]  >= 70 and lista2[i] >= 70:
+#         print(i, end="")
+#         #인덱스 쓰지 않고 위와 같이 써도 ㄱ핸찮
+#         print(" 승진대상")
+    
+
+#함수 : 여러개 명령을 하나로 묶어놈
+#클래스 : 함수와 변수들을 묶어듬
+# 모듈 : 전부 묶어둠, 여러개의 함수들이 import random, turtle
+
+
+
+# def lion(a):
+    
+#     if a < 0: print("양수") 
+#     elif a == 0: print("영")
+#     else : print("양수")
+
+# a = int(input())
+# lion(a)
+
+# def lionking():
+    
+#     a = int(input())
+#     if a < 0: print("양수") 
+#     elif a == 0: print("영")
+#     else : print("양수")
+# lionking()
+
+def mulmul():
+    a = int(input())
+    b = int(input())
+    print(a*b)
+mulmul()
+
+
+a = int(input())
+b = int(input())
+def mulmul2(a,b):
+
+    print(a*b)
+mulmul2(a,b)
+
+
